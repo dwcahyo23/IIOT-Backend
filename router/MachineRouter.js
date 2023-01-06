053fd531630095b3341a430ff0a98a3c6e18239c
Yusuf Dwi <dwcahyo23@gmail.com>
2023-01-06 16:52:04 +0700

    up06-01

--- router/MachineRouter.js
+++ router/MachineRouter.js
@@ -25,11 +25,11 @@ router.get('/machine/:uuid', getMachineIndex);
 router.patch('/machine/:uuid', updateMachineIndex);
 router.delete('/machine/:uuid', deleteMachineIndex);
 
-router.post('/machineitem', postMachineItem);
-router.post('/machineitems', bulkMachineItem);
+router.post('/machineitem', verifyToken, postMachineItem);
+router.post('/machineitems', verifyToken, bulkMachineItem);
 router.get('/machineitems', verifyToken, getMachineItems)
-router.get('/machineitem/:uuid', getMachineItem)
-router.patch('/machineitem/:uuid', updateMachineItem);
-router.delete('/machineitem/:uuid', deleteMachineItem);
+router.get('/machineitem/:uuid', verifyToken, getMachineItem)
+router.patch('/machineitem/:uuid', verifyToken, updateMachineItem);
+router.delete('/machineitem/:uuid', verifyToken, deleteMachineItem);
 
 export default router;
