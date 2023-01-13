9cde87827d06a2f101ec339f76ccaf7a9ddc7b8e
Yusuf Dwi <dwcahyo23@gmail.com>
2023-01-13 14:34:44 +0700

    #130123

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -1,182 +1,11 @@
-import { AuthData, AuthUser } from "../models/AuthModel.js";
-import bcrypt from "bcrypt";
-import _ from "lodash";
+import bcrypt from 'bcrypt';
+import _ from 'lodash';
 
-import Base64 from "crypto-js/enc-base64.js";
-import HmacSHA256 from "crypto-js/hmac-sha256.js";
-import Utf8 from "crypto-js/enc-utf8.js";
-import jwtDecode from "jwt-decode";
-
-const DataUser = AuthUser.hasOne(AuthData, { as: "data" });
-// AuthUser.hasOne(AuthData);
-//AuthData.belongsTo(AuthUser);
-
-export const signUp = async (req, res) => {
-	const { displayName, email, password } = req.body;
-	const salt = await bcrypt.genSalt();
-	const hashPassword = await bcrypt.hash(password, salt);
-	const error = [];
-	try {
-		const isEmailExists = await AuthData.findAll({
-			where: { email: email },
-		});
-
-		if (isEmailExists.length > 0) {
-			error.push({
-				type: "email",
-				message: "The email address is already in use",
-			});
-		}
-
-		if (error.length === 0) {
-			const AuthCreate = await AuthUser.create(
-				{
-					password: hashPassword,
-					role: "admin",
-					data: [
-						{
-							displayName: displayName,
-							photoURL: "",
-							email: email,
-							settings: {
-								layout: {},
-								theme: {},
-							},
-							shortcuts: ["apps.calendar", "apps.mailbox", "apps.contacts"],
-						},
-					],
-				},
-				{
-					include: [DataUser],
-				}
-			);
-
-			const user = await AuthUser.findOne({
-				where: { uuid: AuthCreate.uuid },
-				include: DataUser,
-				attributes: { exclude: ["password"] },
-			});
-
-			const access_token = generateJWTToken({ id: user.uuid });
-
-			const response = {
-				user,
-				access_token,
-			};
-
-			return res.status(200).json(response);
-		}
-
-		return res.status(200).json({ error });
-	} catch (error) {
-		console.log(error.message);
-	}
-};
-
-export const signIn = async (req, res) => {
-	const { email, password } = req.body.data;
-	// const { email, password } = req.body;
-	const error = [];
-	try {
-		const AuthFind = await AuthUser.findAll({
-			include: DataUser,
-		});
-
-		const user = _.cloneDeep(
-			AuthFind.find((_user) => _user.data.email === email)
-		);
-
-		if (!user) {
-			error.push({
-				type: "email",
-				message: "Check your email address",
-			});
-		}
-
-		if (user) {
-			const match = await bcrypt.compare(password, user.password);
-			if (!match) {
-				error.push({
-					type: "password",
-					message: "Check your password",
-				});
-			}
-		}
-
-		if (error.length === 0) {
-			delete user.password;
-
-			const access_token = generateJWTToken({ id: user.uuid });
-
-			const response = {
-				user,
-				access_token,
-			};
-
-			return res.status(200).json(response);
-		}
-
-		return res.status(200).json({ error });
-	} catch (error) {
-		console.log(error.message);
-	}
-};
-
-export const accessToken = async (req, res) => {
-	const { access_token } = req.body.data;
-	try {
-		if (verifyJWTToken(access_token)) {
-			const { id } = jwtDecode(access_token);
-
-			const user = await AuthUser.findOne({
-				where: { uuid: id },
-				include: DataUser,
-				attributes: { exclude: ["password"] },
-			});
-
-			const updatedAccessToken = generateJWTToken({ id: user.uuid });
-
-			const response = {
-				user,
-				access_token: updatedAccessToken,
-			};
-			return res.status(200).json(response);
-		}
-		const error = "Invalid access token detected";
-		return res.status(401).json({ error });
-
-	} catch (error) {
-		console.log(error.message);
-	}
-
-
-};
-
-export const updateApp = async (req,res) => {
-	//console.log(req.body.user.data.settings.theme)
-	//console.log(req.body.user.data.id)
-	try {
-
-		//*! untuk update fungsi get pada auth model harus dihapus */
-		//** cari solusinya... untuk update layout seetttin */
-
-		const response = await AuthData.update({
-			settings: "req.body.user.data.settings"
-		},
-		{
-			where: {
-				id: req.body.user.data.id
-			}
-		})
-		console.log("success")
-		// return res.status(200).json(response)
-		// console.log(user)
-	} catch (error) {
-		console.log(error.message)
-	}
-
-}
-
+import Base64 from 'crypto-js/enc-base64.js';
+import HmacSHA256 from 'crypto-js/hmac-sha256.js';
+import Utf8 from 'crypto-js/enc-utf8.js';
+import jwtDecode from 'jwt-decode';
+import { AuthData, AuthUser } from '../models/AuthModel.js';
 
 /**
  * JWT Token Generator/Verifier Helpers
@@ -184,69 +13,236 @@ export const updateApp = async (req,res) => {
  */
 
 function base64url(source) {
-	// Encode in classical base64
-	let encodedSource = Base64.stringify(source);
+  // Encode in classical base64
+  let encodedSource = Base64.stringify(source);
 
-	// Remove padding equal characters
-	encodedSource = encodedSource.replace(/=+$/, "");
+  // Remove padding equal characters
+  encodedSource = encodedSource.replace(/=+$/, '');
 
-	// Replace characters according to base64url specifications
-	encodedSource = encodedSource.replace(/\+/g, "-");
-	encodedSource = encodedSource.replace(/\//g, "_");
+  // Replace characters according to base64url specifications
+  encodedSource = encodedSource.replace(/\+/g, '-');
+  encodedSource = encodedSource.replace(/\//g, '_');
 
-	// Return the base64 encoded string
-	return encodedSource;
+  // Return the base64 encoded string
+  return encodedSource;
 }
 
 function generateJWTToken(tokenPayload) {
-	// Define token header
-	const header = {
-		alg: "HS256",
-		typ: "JWT",
-	};
+  // Define token header
+  const header = {
+    alg: 'HS256',
+    typ: 'JWT',
+  };
 
-	// Calculate the issued at and expiration dates
-	const date = new Date();
-	const iat = Math.floor(date.getTime() / 1000);
-	const exp = Math.floor(date.setDate(date.getDate() + 7) / 1000);
+  // Calculate the issued at and expiration dates
+  const date = new Date();
+  const iat = Math.floor(date.getTime() / 1000);
+  const exp = Math.floor(date.setDate(date.getDate() + 7) / 1000);
 
-	// Define token payload
-	const payload = {
-		iat,
-		iss: "IIOT",
-		exp,
-		...tokenPayload,
-	};
+  // Define token payload
+  const payload = {
+    iat,
+    iss: 'IIOT',
+    exp,
+    ...tokenPayload,
+  };
 
-	// Stringify and encode the header
-	const stringifiedHeader = Utf8.parse(JSON.stringify(header));
-	const encodedHeader = base64url(stringifiedHeader);
+  // Stringify and encode the header
+  const stringifiedHeader = Utf8.parse(JSON.stringify(header));
+  const encodedHeader = base64url(stringifiedHeader);
 
-	// Stringify and encode the payload
-	const stringifiedPayload = Utf8.parse(JSON.stringify(payload));
-	const encodedPayload = base64url(stringifiedPayload);
+  // Stringify and encode the payload
+  const stringifiedPayload = Utf8.parse(JSON.stringify(payload));
+  const encodedPayload = base64url(stringifiedPayload);
 
-	// Sign the encoded header and mock-api
-	let signature = `${encodedHeader}.${encodedPayload}`;
-	signature = HmacSHA256(signature, process.env.ACCESS_TOKEN_SECRET);
-	signature = base64url(signature);
+  // Sign the encoded header and mock-api
+  let signature = `${encodedHeader}.${encodedPayload}`;
+  signature = HmacSHA256(signature, process.env.ACCESS_TOKEN_SECRET);
+  signature = base64url(signature);
 
-	// Build and return the token
-	return `${encodedHeader}.${encodedPayload}.${signature}`;
+  // Build and return the token
+  return `${encodedHeader}.${encodedPayload}.${signature}`;
 }
 
 function verifyJWTToken(token) {
-	// Split the token into parts
-	const parts = token.split(".");
-	const header = parts[0];
-	const payload = parts[1];
-	const signature = parts[2];
+  // Split the token into parts
+  const parts = token.split('.');
+  const header = parts[0];
+  const payload = parts[1];
+  const signature = parts[2];
 
-	// Re-sign and encode the header and payload using the secret
-	const signatureCheck = base64url(
-		HmacSHA256(`${header}.${payload}`, process.env.ACCESS_TOKEN_SECRET)
-	);
+  // Re-sign and encode the header and payload using the secret
+  const signatureCheck = base64url(
+    HmacSHA256(`${header}.${payload}`, process.env.ACCESS_TOKEN_SECRET),
+  );
 
-	// Verify that the resulting signature is valid
-	return signature === signatureCheck;
+  // Verify that the resulting signature is valid
+  return signature === signatureCheck;
 }
+
+const DataUser = AuthUser.hasOne(AuthData, { as: 'data' });
+// AuthUser.hasOne(AuthData);
+// AuthData.belongsTo(AuthUser);
+
+export const signUp = async (req, res) => {
+  const { displayName, email, password } = req.body;
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
+  const error = [];
+  try {
+    const isEmailExists = await AuthData.findAll({
+      where: { email },
+    });
+
+    if (isEmailExists.length > 0) {
+      error.push({
+        type: 'email',
+        message: 'The email address is already in use',
+      });
+    }
+
+    if (error.length === 0) {
+      const AuthCreate = await AuthUser.create(
+        {
+          password: hashPassword,
+          role: 'admin',
+          data: [
+            {
+              displayName,
+              photoURL: '',
+              email,
+              settings: {
+                layout: {},
+                theme: {},
+              },
+              shortcuts: ['apps.calendar', 'apps.mailbox', 'apps.contacts'],
+            },
+          ],
+        },
+        {
+          include: [DataUser],
+        },
+      );
+
+      const user = await AuthUser.findOne({
+        where: { uuid: AuthCreate.uuid },
+        include: DataUser,
+        attributes: { exclude: ['password'] },
+      });
+
+      const access_token = generateJWTToken({ id: user.uuid });
+
+      const response = {
+        user,
+        access_token,
+      };
+
+      return res.status(200).json(response);
+    }
+
+    return res.status(200).json({ error });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const signIn = async (req, res) => {
+  const { email, password } = req.body.data;
+  // const { email, password } = req.body;
+  const error = [];
+  try {
+    const AuthFind = await AuthUser.findAll({
+      include: DataUser,
+    });
+
+    const user = _.cloneDeep(
+      AuthFind.find((_user) => _user.data.email === email),
+    );
+
+    if (!user) {
+      error.push({
+        type: 'email',
+        message: 'Check your email address',
+      });
+    }
+
+    if (user) {
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        error.push({
+          type: 'password',
+          message: 'Check your password',
+        });
+      }
+    }
+
+    if (error.length === 0) {
+      delete user.password;
+
+      const access_token = generateJWTToken({ id: user.uuid });
+
+      const response = {
+        user,
+        access_token,
+      };
+
+      return res.status(200).json(response);
+    }
+
+    return res.status(200).json({ error });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const accessToken = async (req, res) => {
+  const { access_token } = req.body.data;
+  try {
+    if (verifyJWTToken(access_token)) {
+      const { id } = jwtDecode(access_token);
+
+      const user = await AuthUser.findOne({
+        where: { uuid: id },
+        include: DataUser,
+        attributes: { exclude: ['password'] },
+      });
+
+      const updatedAccessToken = generateJWTToken({ id: user.uuid });
+
+      const response = {
+        user,
+        access_token: updatedAccessToken,
+      };
+      return res.status(200).json(response);
+    }
+    const error = 'Invalid access token detected';
+    return res.status(401).json({ error });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const updateApp = async (req, res) => {
+  // console.log(req.body.user.data.settings.theme)
+  // console.log(req.body.user.data.id)
+  try {
+    //* ! untuk update fungsi get pada auth model harus dihapus */
+    //* * cari solusinya... untuk update layout seetttin */
+
+    const response = await AuthData.update(
+      {
+        settings: 'req.body.user.data.settings',
+      },
+      {
+        where: {
+          id: req.body.user.data.id,
+        },
+      },
+    );
+    console.log('success');
+    // return res.status(200).json(response)
+    // console.log(user)
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- controllers/MachineControllers.js
+++ controllers/MachineControllers.js
@@ -1,4 +1,4 @@
-import { MachineIndex, MachineItems } from "../models/MachineModel.js";
+import { MachineIndex, MachineItems } from '../models/MachineModel.js';
 
 // const DataItems = MachineIndex.hasMany(MachineItems, { as: 'data' });
 
@@ -6,166 +6,199 @@ MachineIndex.hasMany(MachineItems);
 MachineItems.belongsTo(MachineIndex);
 
 export const postMachineIndex = async (req, res) => {
-	const { mch_code, mch_name, mch_com, mch_loc } = req.body;
-	const error = [];
-	try {
-		const response = await MachineIndex.create({
-			mch_code, mch_name, mch_com, mch_loc
-		})
-		res.status(200).json(response)
-
-	} catch (error) {
-		res.status(400).json(error.message)
-		console.log(error.message)
-	}
-}
+  const {
+    mch_code, mch_name, mch_com, mch_loc,
+  } = req.body;
+  const error = [];
+  try {
+    const response = await MachineIndex.create({
+      mch_code, mch_name, mch_com, mch_loc,
+    });
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(400).json(err.message);
+    console.log(err.message);
+  }
+};
 
 export const bulkMachineIndex = async (req, res) => {
-	console.log(req.body)
-	const data = req.body
-	try {
-		const response = await MachineIndex.bulkCreate(data,
-			{ validate: true },
-			{ fields: ['mch_code', 'mch_name', 'mch_process', 'mch_com', 'mch_loc'] })
-		res.status(200).json(response)
-	} catch (error) {
-		console.log(error);
-	}
-}
+  console.log(req.body);
+  const data = req.body;
+  try {
+    const response = await MachineIndex.bulkCreate(
+      data,
+      { validate: true },
+      { fields: ['mch_code', 'mch_name', 'mch_process', 'mch_com', 'mch_loc'] },
+    );
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getMachines = async (req, res) => {
-	try {
-		const response = await MachineIndex.findAll({})
-		res.status(200).json(response)
-	} catch (error) {
-		console.log(error.message);
-	}
-}
-
+export const getMachines = async (_req, res) => {
+  try {
+    const response = await MachineIndex.findAll({});
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const getMachineIndex = async (req, res) => {
-	try {
-		const response = await MachineIndex.findOne({
-			where: {
-				uuid: req.params.uuid
-			}
-		})
-		res.status(200).json(response)
-	} catch (error) {
-		console.log(error.message);
-	}
-}
+  try {
+    const response = await MachineIndex.findOne({
+      where: {
+        uuid: req.params.uuid,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const updateMachineIndex = async (req, res) => {
-	const data = req.body;
-	try {
-		const response = await MachineIndex.update(data,
-			{
-				where: {
-					uuid: req.params.uuid
-				}
-			})
-		res.status(200).json(response)
-	} catch (error) {
-		console.log(error.message);
-	}
-}
+  const data = req.body;
+  try {
+    const response = await MachineIndex.update(
+      data,
+      {
+        where: {
+          uuid: req.params.uuid,
+        },
+      },
+    );
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-export const deleteMachineIndex = async (req, res) => {
-	try {
-		const response = await MachineIndex.destroy({
-			where: {
-				uuid: req.params.uuid
-			}
-		})
-	} catch (error) {
-
-	}
-}
+export const deleteMachineIndex = async (req, _res) => {
+  try {
+    const response = await MachineIndex.destroy({
+      where: {
+        uuid: req.params.uuid,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //* Edit by form
 export const bulkMachineItem = async (req, res) => {
-	console.log(req.body)
-	const data = req.body
-	try {
-		const response = await MachineItems.bulkCreate(data)
-		res.status(200).json(response)
-	} catch (error) {
-		console.log(error.message);
-	}
-}
+  console.log(req.body);
+  const data = req.body;
+  try {
+    const response = await MachineItems.bulkCreate(data);
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const getMachineItem = async (req, res) => {
-	try {
-		const response = await MachineItems.findOne({
-			where: {
-				uuid: req.params.uuid
-			},
-			include: MachineIndex,
-		})
-		res.status(200).json(response)
-	} catch (error) {
-		console.log(error.message);
-	}
-}
+  try {
+    const response = await MachineItems.findOne({
+      where: {
+        uuid: req.params.uuid,
+      },
+      include: MachineIndex,
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const getMachineItems = async (req, res) => {
-	try {
-		const response = await MachineItems.findAll({ include: MachineIndex })
-		res.status(200).json(response)
-	} catch (error) {
-		console.log(error.message);
-	}
-}
+  try {
+    const response = await MachineItems.findAll({ include: MachineIndex });
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const updateMachineItem = async (req, res) => {
-	const { bom, category, item_name, item_life_time, item_lead_time, item_status, machineIndexUuid, uuid } = req.body;
-	try {
-		const isItemExists = await MachineItems.findAll({
-			where: { uuid: uuid },
-		});
-		console.log(isItemExists.length)
+  const {
+    bom,
+    category,
+    item_name,
+    item_life_time,
+    item_lead_time,
+    item_status,
+    machineIndexUuid,
+    uuid,
+    featuredImageId,
+    images,
+  } = req.body;
+  const isItemExists = await MachineItems.findAll({
+    where: { uuid },
+    include: MachineIndex,
+  });
 
-		if (isItemExists.length > 0) {
-			const update = await MachineItems.update({ bom, category, item_name, item_life_time, item_lead_time, item_status, machineIndexUuid },
-				{
-					where: {
-						uuid: req.params.uuid
-					}
-				})
-			const response = await MachineItems.findOne({
-				where: {
-					uuid: req.params.uuid
-				},
-				include: MachineIndex,
-			})
-			return res.status(200).json(response)
-		} else {
-			const create = await MachineItems.create({ bom, category, item_name, item_life_time, item_lead_time, item_status, machineIndexUuid });
-			const response = await MachineItems.findOne({
-				where: {
-					uuid: req.params.uuid
-				},
-				include: MachineIndex,
-			})
-			return res.status(200).json(response)
-		}
-
-	} catch (error) {
-		console.log(error);
-	}
-}
+  if (isItemExists.length > 0) {
+    await MachineItems.update({
+      bom,
+      category,
+      item_name,
+      item_lead_time,
+      item_life_time,
+      item_status,
+      machineIndexUuid,
+      images,
+      featuredImageId,
+    }, {
+      where: {
+        uuid,
+      },
+      sideEffects: false,
+    }).then(() => {
+      MachineItems.findOne({
+        where: { uuid },
+        include: MachineIndex,
+      });
+    }).then((result) => {
+      res.status(200).json(result);
+    }).catch((err) => {
+      console.log(err);
+    });
+  } else {
+    await MachineItems.create({
+      bom,
+      category,
+      item_name,
+      item_life_time,
+      item_lead_time,
+      item_status,
+      machineIndexUuid,
+      images,
+    }).then(() => {
+      MachineItems.findOne({
+        where: { uuid },
+        include: MachineIndex,
+      });
+    }).then((result) => {
+      res.status(200).send(result);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+};
 
 export const deleteMachineItem = async (req, res) => {
-	const uuid = req.body
-	try {
-		const response = await MachineItems.destroy({
-			where: {
-				uuid: uuid
-			}
-		})
-		res.status(200).json(response)
-	} catch (error) {
-		console.log(error.message);
-	}
-}
+  const uuid = req.body;
+  try {
+    const response = await MachineItems.destroy({
+      where: {
+        uuid,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
