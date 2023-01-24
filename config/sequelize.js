d1810cbf89ebcf26715bbd350adfc6ce96922a90
Yusuf Dwi <dwcahyo23@gmail.com>
2023-01-24 11:25:40 +0700

    #240123-1

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import * as dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(`dirname: ${path.join(__dirname, '../.env')}`);
+
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const sequelize = new Sequelize(process.env.TABLE, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+/* eslint-disable no-underscore-dangle */
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import express from "express";
-import cors from "cors";
-import AuthRouter from "./router/AuthRouter.js";
-import MachineRouter from "./router/MachineRouter.js"
+import * as dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+// console.log(`dirname: ${path.join(__dirname, '../.env')}`);
+import express from 'express';
+import cors from 'cors';
+import AuthRouter from './router/AuthRouter.js';
+import MachineRouter from './router/MachineRouter.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const corsOptions = {
-	origin: 'http://localhost:3000', // ini IP frontendmu
-	optionsSuccessStatus: 200
-}
+  origin: 'http://localhost:3000', // ini IP frontendmu
+  optionsSuccessStatus: 200,
+};
 
-const port = 5000
+const port = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -20,7 +28,4 @@ app.use(express.urlencoded({ limit: '50mb' }));
 app.use(AuthRouter);
 app.use(MachineRouter);
 
-
-
-
-app.listen(port, () => console.log(`Server up & running in ${port}`))
+app.listen(port, () => console.log(`Server up & running in ${port}`));
