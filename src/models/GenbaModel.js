0ec928ec101f4861912c5f09219e935e988a7169
Yusuf Dwi <dwcahyo23@gmail.com>
2023-08-07 15:59:09 +0700

    #070823

--- src/models/GenbaModel.js
+++ src/models/GenbaModel.js
@@ -30,13 +30,17 @@ export const GenbaAcip = sequelize.define('GenbaAcip', {
     images2: {
         type: DataTypes.JSON,
         defaultValue: [],
-        // get: function () {
-        //     if (typeof this.getDataValue('images2') == 'string') {
-        //         return JSON.parse(this.getDataValue('images2'))
-        //     } else {
-        //         return this.getDataValue('images2')
-        //     }
-        // },
+        get: function () {
+            if (typeof this.getDataValue('images2') == 'string') {
+                return JSON.parse(this.getDataValue('images2'))
+            } else {
+                return this.getDataValue('images2')
+            }
+        },
+        set: function (value) {
+            // const gzippedBuffer = gzipSync(value);
+            this.setDataValue('images2', value)
+        },
     },
     a_r1: DataTypes.INTEGER,
     a_r2: DataTypes.INTEGER,
