46b1c8b414181ecbf1bae70198034915b5b957e8
Yusuf Dwi <dwcahyo23@gmail.com>
2023-10-10 11:56:50 +0700

    #101023--verifytokenmns

--- src/router/MaintenanceSystemRouter.js
+++ src/router/MaintenanceSystemRouter.js
@@ -3,48 +3,69 @@ import { verifyToken } from '../middleware/verifyToken.js'
 
 export default (app) => {
     //!@noone
-    // app.get('/maintenanceCategory', Mn.getMaintenanceCategory)
-    // app.post('/maintenanceCategory', Mn.insMaintenanceCategory)
+    // app.get('/maintenanceCategory',verifyToken, Mn.getMaintenanceCategory)
+    // app.post('/maintenanceCategory',verifyToken, Mn.insMaintenanceCategory)
 
     //!false
-    // app.get('/maintenanceMachineOne/:uuid', Mn.getMaintenanceMachineByOne)
-    // app.get('/pgMaintenance', Mn.pGMaintenance)
-    // app.post('/maintenanceStock', Mn.insMaintenanceStock)
-    // app.post('/maintenanceSparepart', Mn.insMaintenanceSparepart)
-    // app.post('/maintenanceWorkshopReport', Mn.instMaintenanceBulkWorkshopReport)
-    // app.post('/maintenanceMachine', Mn.insMaintenanceMachine)
-    // app.post('/maintenanceMachine/update', Mn.updateMaintenanceMachine)
-    // app.post('/maintenanceControlStockB', Mn.instMaintenanceBulkControllStock)
-    // app.post('/maintenanceReportB', Mn.instMaintenanceBulkReport)
-    // app.post('/maintenanceRequestB', Mn.instMaintenanceBulkRequest)
+    // app.get('/maintenanceMachineOne/:uuid',verifyToken, Mn.getMaintenanceMachineByOne)
+    // app.get('/pgMaintenance',verifyToken, Mn.pGMaintenance)
+    // app.post('/maintenanceStock',verifyToken, Mn.insMaintenanceStock)
+    // app.post('/maintenanceSparepart',verifyToken, Mn.insMaintenanceSparepart)
+    // app.post('/maintenanceWorkshopReport',verifyToken, Mn.instMaintenanceBulkWorkshopReport)
+    // app.post('/maintenanceMachine',verifyToken, Mn.insMaintenanceMachine)
+    // app.post('/maintenanceMachine/update',verifyToken, Mn.updateMaintenanceMachine)
+    // app.post('/maintenanceControlStockB',verifyToken, Mn.instMaintenanceBulkControllStock)
+    // app.post('/maintenanceReportB',verifyToken, Mn.instMaintenanceBulkReport)
+    // app.post('/maintenanceRequestB',verifyToken, Mn.instMaintenanceBulkRequest)
     //Menu dashboard dialog AP-User
-    // app.post('/maintenanceSparepartBulk', Mn.insMaintenanceSparepartBulkFind)
+    // app.post('/maintenanceSparepartBulk',verifyToken, Mn.insMaintenanceSparepartBulkFind)
 
     //!@get
-    app.get('/maintenanceMachine/:uuid', Mn.getMaintenanceMachineBy)
-    app.get('/maintenanceStock', Mn.getMaintenanceStock)
-    app.get('/maintenanceMachineCom', Mn.getMaintenanceMachineCom)
-    app.get('/maintenanceMachineProcess', Mn.getMaintenanceMachineProcess)
-    app.get('/maintenanceSystem', Mn.getMaintenanceSystem)
-    app.get('/pgMaintenance', Mn.pGMaintenance)
-    app.get('/maintenanceDashboard/:com/:section', Mn.dashboardMN)
-    app.get('/maintenanceControlStock', Mn.getMaintenanceControlStock)
-    app.get('/maintenanceMachine', Mn.getMaintenanceMachine)
+    app.get(
+        '/maintenanceMachine/:uuid',
+        verifyToken,
+        Mn.getMaintenanceMachineBy
+    )
+    app.get('/maintenanceStock', verifyToken, Mn.getMaintenanceStock)
+    app.get('/maintenanceMachineCom', verifyToken, Mn.getMaintenanceMachineCom)
+    app.get(
+        '/maintenanceMachineProcess',
+        verifyToken,
+        Mn.getMaintenanceMachineProcess
+    )
+    app.get('/maintenanceSystem', verifyToken, Mn.getMaintenanceSystem)
+    app.get('/pgMaintenance', verifyToken, Mn.pGMaintenance)
+    app.get('/maintenanceDashboard/:com/:section', verifyToken, Mn.dashboardMN)
+    app.get(
+        '/maintenanceControlStock',
+        verifyToken,
+        Mn.getMaintenanceControlStock
+    )
+    app.get('/maintenanceMachine', verifyToken, Mn.getMaintenanceMachine)
     app.get(
         '/machineSheet/:uuid/:sheet_no/:uuid_request',
         Mn.getMaintenanceMachineBySheet
     )
-    app.get('/maintenanceReport', Mn.getMaintenanceReport)
-    app.get('/maintenanceRequest', Mn.getMaintenanceRequest)
-    app.get('/userData', Mn.getUser)
+    app.get('/maintenanceReport', verifyToken, Mn.getMaintenanceReport)
+    app.get('/maintenanceRequest', verifyToken, Mn.getMaintenanceRequest)
+    app.get('/userData', verifyToken, Mn.getUser)
 
     //!@post
-    app.post('/maintenanceReport', Mn.insMaintenanceReport)
-    // app.post('/maintenanceRequest', Mn.insMaintenanceRequest)
-    app.post('/maintenanceControlStock', Mn.instMaintenanceControlStock)
+    app.post('/maintenanceReport', verifyToken, Mn.insMaintenanceReport)
+    // app.post('/maintenanceRequest',verifyToken, Mn.insMaintenanceRequest)
+    app.post(
+        '/maintenanceControlStock',
+        verifyToken,
+        Mn.instMaintenanceControlStock
+    )
     app.post(
         '/maintenanceControlUpdateStock',
+        verifyToken,
         Mn.updateStockMaintenanceControlStock
     )
-    app.post('/maintenanceRequest/:options/:user', Mn.handleMaintenanceRequest)
+    app.post(
+        '/maintenanceRequest/:options/:user',
+        verifyToken,
+        Mn.handleMaintenanceRequest
+    )
 }
