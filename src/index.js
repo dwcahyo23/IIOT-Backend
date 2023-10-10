f5ea4e8b2459c9cab5dc3c02c4b35258d5892baa
Yusuf Dwi <dwcahyo23@gmail.com>
2023-10-10 12:20:14 +0700

    #101023 --add helmet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv'
 
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
 import AuthRouter from './router/AuthRouter.js'
 import Genba from './router/GenbaRouter.js'
 import ModbusAppRouter from './router/ModbusAppRouter.js'
@@ -12,7 +13,14 @@ import ZviewRouter from './router/ZviewRouter.js'
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(
+    cors({
+        methods: ['GET', 'POST', 'DELETE'],
+        allowedHeaders: ['Authorization', 'Content-Type'],
+    })
+)
+app.use(helmet())
+
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(AuthRouter)
