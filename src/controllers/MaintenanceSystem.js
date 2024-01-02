21f0337076d77fc1d24e8929e6f49a2ba1ccda40
Yusuf Dwi <dwcahyo23@gmail.com>
2024-01-02 09:06:10 +0700

    #020124

--- src/controllers/MaintenanceSystem.js
+++ src/controllers/MaintenanceSystem.js
@@ -1172,23 +1172,24 @@ export default {
             return new Promise((resolve, reject) => {
                 const useMixing = _(material)
                     .map((val) => {
-                        return {
-                            ...val,
-                            mch_index: _.find(
-                                spice,
-                                {
-                                    mch_code: val.mch_no,
-                                    mch_com:
-                                        val.com_no == '01'
-                                            ? 'GM1'
-                                            : val.com_no == '02'
-                                            ? 'GM2'
-                                            : val.com_no == '03'
-                                            ? 'GM3'
-                                            : 'GM5',
-                                } || {}
-                            ),
-                        }
+                        if (dayjs(val.ymd).year() >= '2023')
+                            return {
+                                ...val,
+                                mch_index: _.find(
+                                    spice,
+                                    {
+                                        mch_code: val.mch_no,
+                                        mch_com:
+                                            val.com_no == '01'
+                                                ? 'GM1'
+                                                : val.com_no == '02'
+                                                ? 'GM2'
+                                                : val.com_no == '03'
+                                                ? 'GM3'
+                                                : 'GM5',
+                                    } || {}
+                                ),
+                            }
                     })
                     .groupBy((val) => val.mch_index?.responsible)
                     .omit(['undefined'])
